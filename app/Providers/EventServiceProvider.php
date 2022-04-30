42e53384af5c6b4397295c128039167ae1a8805a
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2022-04-30 15:18:43 +0700

    fix: add use App Listener UserEventSubscriber

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Events\LoginHistory;
 use App\Listeners\StoreUserLoginHistory;
+use App\Listeners\UserEventSubscriber;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
